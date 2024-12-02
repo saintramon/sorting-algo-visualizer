3cd1a87de47afec34ac6ce876ec3a8bf5404e1bb
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2024-12-02 20:45:35 +0800

    Added bare structure of components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Visualizer from './Visualizer';
+
+const App = () => {
+    return(
+        <>
+            <Visualizer/>
+        </>
+    );
+}
+
+export default App;
