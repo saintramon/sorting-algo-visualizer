62ac9850e176e5787dacc4b303a8d57203c14fd6
saintramon <ramonjasmin8@gmail.com>
2024-12-17 22:21:38 +0800

    Added initial bars and intiial styling

--- src/visualizer/Visualizer.jsx
+++ src/visualizer/Visualizer.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import './Visualizer.css'
 
-const Visualizer = () => {
+const Visualizer = ({numBars, speed, algorithm}) => {
+
+    const [array, setArray] = useState([])
+
+    const genInitialArray = () => {
+        const arr = []; 
+    
+        for(let i = 0; i < numBars; i++){
+            arr.push(Math.floor(Math.random() * 100) + 1) //Generates a number from 1 to 100
+        }
+
+        setArray(arr);
+    };
+
+    useEffect(() => {
+        genInitialArray();
+    }, [numBars]);
+    
+
     return(
-        <>
-            <h1>THIS IS THE VISUALIZER COMPONENT</h1>
-        </>
+        <div className='array-container'>
+            {array.map((value, index) => (
+                <div key={index} className='array-bar' style={{
+                    height: `${value}px`
+                }}>
+                </div>
+            ))}
+        </div>
     );
 }
 
